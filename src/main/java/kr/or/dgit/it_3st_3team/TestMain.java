20fede394b52d853eb92a9348cdbf017e60092bd
GangDae Lee <enhoomin@naver.com>
2018-02-19 09:28:21 +0900

    mergy 수정

--- src/main/java/kr/or/dgit/it_3st_3team/TestMain.java
+++ src/main/java/kr/or/dgit/it_3st_3team/TestMain.java
@@ -2,10 +2,6 @@ package kr.or.dgit.it_3st_3team;
 
 public class TestMain {
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println("IT융합 3기 3조");
-=======
-		System.out.println("야호");
->>>>>>> refs/remotes/origin/master
 	}
 }
