bff75f8eedff65b72f376aca5ab2ee21176dfe1d
HyeSeon Yang <skcm19@naver.com>
2018-02-22 17:09:56 +0900

    ComboboxComponent 수정

--- src/main/java/kr/or/dgit/it_3st_3team/ui/component/SoftwareComboboxComponent.java
+++ src/main/java/kr/or/dgit/it_3st_3team/ui/component/SoftwareComboboxComponent.java
@@ -1,11 +1,15 @@
 package kr.or.dgit.it_3st_3team.ui.component;
 
-public class SoftwareComboboxComponent extends AbstractCombobox<String> {
+import kr.or.dgit.it_3st_3team.dto.SoftwareGroup;
+
+public class SoftwareComboboxComponent extends AbstractCombobox<SoftwareGroup> {
 
 	
 	public SoftwareComboboxComponent() {
 
 	}
+
+	
 
 
 }
