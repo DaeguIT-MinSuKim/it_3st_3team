3ac4ef306189b450f9eb2155ed5d03eb0b59b917
GangDae Lee <enhoomin@naver.com>
2018-02-24 00:49:04 +0900

    담당자 라벨 있는 콤보박스 추가

---
+++ src/main/java/kr/or/dgit/it_3st_3team/ui/component/LblCmbAdminComp.java
@@ -0,0 +1,13 @@
+package kr.or.dgit.it_3st_3team.ui.component;
+
+
+import kr.or.dgit.it_3st_3team.dto.Admin;
+
+@SuppressWarnings("serial")
+public class LblCmbAdminComp extends AbstractLabelComboBoxComp<Admin> {
+
+	public LblCmbAdminComp(String title) {
+		super(title);
+	}
+
+}
