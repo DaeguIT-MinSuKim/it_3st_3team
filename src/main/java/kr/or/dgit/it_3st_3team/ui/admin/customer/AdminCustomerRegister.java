7737776e71f0488133a5f01767609d38c2829f39
GangDae Lee <enhoomin@naver.com>
2018-02-23 17:37:50 +0900

    UI 수정 및 정리

--- src/main/java/kr/or/dgit/it_3st_3team/ui/admin/customer/AdminCustomerRegister.java
+++ src/main/java/kr/or/dgit/it_3st_3team/ui/admin/customer/AdminCustomerRegister.java
@@ -1,135 +1,72 @@
 package kr.or.dgit.it_3st_3team.ui.admin.customer;
 
-import javax.swing.JPanel;
-import javax.swing.JLabel;
-import javax.swing.SwingConstants;
-import javax.swing.JTextField;
-import javax.swing.JPasswordField;
-import javax.swing.JButton;
 import java.awt.Color;
 
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
 import kr.or.dgit.it_3st_3team.ui.component.ImageComp;
+import kr.or.dgit.it_3st_3team.ui.component.LblAddressComp;
+import kr.or.dgit.it_3st_3team.ui.component.LblPwdTfComp;
+import kr.or.dgit.it_3st_3team.ui.component.LblTfBtnComp;
+import kr.or.dgit.it_3st_3team.ui.component.LblTfComp;
 
 @SuppressWarnings("serial")
 public class AdminCustomerRegister extends JPanel {
-	private JTextField textField;
-	private JPasswordField passwordField;
-	private JPasswordField passwordField_1;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
-	private JTextField textField_4;
-	private JTextField textField_5;
 
 	public AdminCustomerRegister() {
 		initComponents();
 	}
 
 	private void initComponents() {
-		setBackground(Color.WHITE);
 		setLayout(null);
+		setBackground(new Color(255, 255, 255));
+		setBounds(0, 0, 1178, 250);
 				
 		ImageComp pImgArea = new ImageComp();
-		pImgArea.setBounds(0, 10, 200, 230);
+		pImgArea.setBounds(12, 10, 200, 230);
 		add(pImgArea);
 
 		JPanel panel_2 = new JPanel();
-		panel_2.setBounds(212, 10, 552, 230);
+		panel_2.setBackground(new Color(255, 255, 255));
+		panel_2.setBounds(235, 10, 931, 230);
 		panel_2.setLayout(null);
 		add(panel_2);
 
-		JLabel label = new JLabel("아이디");
-		label.setHorizontalAlignment(SwingConstants.RIGHT);
-		label.setBounds(44, 12, 48, 15);
-		panel_2.add(label);
-
-		textField = new JTextField(20);
-		textField.setHorizontalAlignment(SwingConstants.LEFT);
-		textField.setBounds(104, 7, 146, 25);
-		panel_2.add(textField);
-
-		JLabel label_1 = new JLabel("비밀번호");
-		label_1.setHorizontalAlignment(SwingConstants.RIGHT);
-		label_1.setBounds(12, 42, 80, 15);
-		panel_2.add(label_1);
-
-		passwordField = new JPasswordField();
-		passwordField.setBounds(104, 37, 146, 25);
-		panel_2.add(passwordField);
-
-		JLabel label_2 = new JLabel("비밀번호 확인");
-		label_2.setHorizontalAlignment(SwingConstants.RIGHT);
-		label_2.setBounds(280, 42, 90, 15);
-		panel_2.add(label_2);
-
-		passwordField_1 = new JPasswordField();
-		passwordField_1.setBounds(382, 37, 146, 25);
-		panel_2.add(passwordField_1);
-
-		JLabel label_3 = new JLabel("상호명");
-		label_3.setHorizontalAlignment(SwingConstants.RIGHT);
-		label_3.setBounds(44, 72, 48, 15);
-		panel_2.add(label_3);
-
-		textField_1 = new JTextField();
-		textField_1.setHorizontalAlignment(SwingConstants.LEFT);
-		textField_1.setColumns(10);
-		textField_1.setBounds(104, 67, 146, 25);
-		panel_2.add(textField_1);
-
-		JLabel label_4 = new JLabel("전화번호");
-		label_4.setHorizontalAlignment(SwingConstants.RIGHT);
-		label_4.setBounds(322, 72, 48, 15);
-		panel_2.add(label_4);
-
-		textField_2 = new JTextField();
-		textField_2.setHorizontalAlignment(SwingConstants.LEFT);
-		textField_2.setColumns(10);
-		textField_2.setBounds(382, 67, 146, 25);
-		panel_2.add(textField_2);
-
-		JLabel label_5 = new JLabel("우편번호");
-		label_5.setHorizontalAlignment(SwingConstants.RIGHT);
-		label_5.setBounds(44, 100, 48, 15);
-		panel_2.add(label_5);
-
-		textField_3 = new JTextField();
-		textField_3.setHorizontalAlignment(SwingConstants.LEFT);
-		textField_3.setColumns(10);
-		textField_3.setBounds(104, 97, 146, 25);
-		panel_2.add(textField_3);
-
-		JLabel label_6 = new JLabel("주소");
-		label_6.setHorizontalAlignment(SwingConstants.RIGHT);
-		label_6.setBounds(44, 130, 48, 15);
-		panel_2.add(label_6);
-
-		textField_4 = new JTextField();
-		textField_4.setHorizontalAlignment(SwingConstants.LEFT);
-		textField_4.setColumns(10);
-		textField_4.setBounds(104, 127, 340, 25);
-		panel_2.add(textField_4);
-
-		JButton button = new JButton("중복확인");
-		button.setBounds(262, 7, 80, 25);
-		panel_2.add(button);
-
-		JButton button_1 = new JButton("검색");
-		button_1.setBounds(262, 97, 80, 25);
-		panel_2.add(button_1);
-
-		textField_5 = new JTextField();
-		textField_5.setHorizontalAlignment(SwingConstants.LEFT);
-		textField_5.setColumns(10);
-		textField_5.setBounds(104, 157, 340, 25);
-		panel_2.add(textField_5);
-
 		JButton button_2 = new JButton("등록");
-		button_2.setBounds(322, 197, 97, 30);
+		button_2.setBounds(654, 190, 97, 30);
 		panel_2.add(button_2);
 
 		JButton button_3 = new JButton("취소");
-		button_3.setBounds(431, 197, 97, 30);
+		button_3.setBounds(783, 190, 97, 30);
 		panel_2.add(button_3);
+		
+		LblTfBtnComp panel = new LblTfBtnComp("아이디", "중복확인");
+		panel.setBounds(103, 10, 288, 30);
+		panel_2.add(panel);
+		
+		LblPwdTfComp panel_1 = new LblPwdTfComp("비밀번호");
+		panel_1.setBounds(91, 50, 300, 30);
+		panel_2.add(panel_1);
+		
+		LblPwdTfComp lblPwdTfComp = new LblPwdTfComp("비밀번호 확인");
+		lblPwdTfComp.setBounds(63, 90, 291, 30);
+		panel_2.add(lblPwdTfComp);
+		
+		LblTfComp panel_3 = new LblTfComp("상호명");
+		panel_3.setBounds(103, 130, 288, 30);
+		panel_2.add(panel_3);
+		
+		LblTfComp lblTfComp = new LblTfComp("전화번호");
+		lblTfComp.setBounds(91, 170, 263, 30);
+		panel_2.add(lblTfComp);
+		
+		LblTfBtnComp lblTfBtnComp = new LblTfBtnComp("우편번호", "검색");
+		lblTfBtnComp.setBounds(514, 50, 280, 30);
+		panel_2.add(lblTfBtnComp);
+		
+		LblAddressComp panel_4 = new LblAddressComp("주소");
+		panel_4.setBounds(538, 90, 342, 70);
+		panel_2.add(panel_4);
 	}
 }
--- src/main/java/kr/or/dgit/it_3st_3team/ui/admin/order/AdminOrderSearch.java
+++ src/main/java/kr/or/dgit/it_3st_3team/ui/admin/order/AdminOrderSearch.java
@@ -1,56 +1,50 @@
 package kr.or.dgit.it_3st_3team.ui.admin.order;
 
-import javax.swing.JPanel;
 import java.awt.Color;
-import javax.swing.JComboBox;
-import javax.swing.JTextField;
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.SwingConstants;
-import kr.or.dgit.it_3st_3team.ui.component.CmbSoftwareComp;
-import kr.or.dgit.it_3st_3team.ui.component.CmbSoftwareGroupComp;
+
+import javax.swing.JPanel;
+
 import kr.or.dgit.it_3st_3team.ui.component.CmbAdminGroupComp;
-import kr.or.dgit.it_3st_3team.ui.component.StartAndEndDate;
+import kr.or.dgit.it_3st_3team.ui.component.CmbSoftwareGroupComp;
 import kr.or.dgit.it_3st_3team.ui.component.CmbStringComp;
-import kr.or.dgit.it_3st_3team.ui.component.TfBtnSearchPrintComp;
+import kr.or.dgit.it_3st_3team.ui.component.StartAndEndDate;
 import kr.or.dgit.it_3st_3team.ui.component.TfBtnSearchComp;
 
+@SuppressWarnings("serial")
 public class AdminOrderSearch extends JPanel {
 
-	/**
-	 * Create the panel.
-	 */
 	public AdminOrderSearch() {
 
 		initComponents();
 	}
+
 	private void initComponents() {
 		setLayout(null);
-		
+
 		JPanel pOrderSearch = new JPanel();
 		pOrderSearch.setBackground(Color.WHITE);
 		pOrderSearch.setBounds(0, 0, 1190, 94);
 		add(pOrderSearch);
 		pOrderSearch.setLayout(null);
-		
+
 		CmbSoftwareGroupComp pSearchSoftwareSort = new CmbSoftwareGroupComp();
 		pSearchSoftwareSort.setBounds(168, 34, 120, 30);
 		pOrderSearch.add(pSearchSoftwareSort);
-		
+
 		CmbAdminGroupComp pAdminGroup = new CmbAdminGroupComp();
 		pAdminGroup.setBounds(325, 34, 120, 30);
 		pOrderSearch.add(pAdminGroup);
-		
+
 		StartAndEndDate pDate = new StartAndEndDate();
 		pDate.setBackground(Color.WHITE);
 		pDate.setBounds(488, 10, 690, 43);
 		pOrderSearch.add(pDate);
-		
+
 		CmbStringComp pSortUserNameSWName = new CmbStringComp();
 		pSortUserNameSWName.setBounds(493, 54, 120, 30);
 		pOrderSearch.add(pSortUserNameSWName);
-		
-		TfBtnSearchComp pSearch = new TfBtnSearchComp();
+
+		TfBtnSearchComp pSearch = new TfBtnSearchComp("검색");
 		pSearch.setBounds(638, 54, 540, 30);
 		pOrderSearch.add(pSearch);
 	}
