b55b2e7a617aeea24a63ae6d604a011ac96db7df
GangDae Lee <enhoomin@naver.com>
2018-02-23 18:47:18 +0900

    관리자 주문 관리 공용 사용을 위한 수정

--- src/main/java/kr/or/dgit/it_3st_3team/ui/admin/order/AdminOrderSearch.java
+++ src/main/java/kr/or/dgit/it_3st_3team/ui/admin/order/AdminOrderSearch.java
@@ -23,29 +23,29 @@ public class AdminOrderSearch extends JPanel {
 
 		JPanel pOrderSearch = new JPanel();
 		pOrderSearch.setBackground(Color.WHITE);
-		pOrderSearch.setBounds(0, 0, 1200, 94);
+		pOrderSearch.setBounds(0, 0, 1200, 50);
 		add(pOrderSearch);
 		pOrderSearch.setLayout(null);
 
 		CmbSoftwareGroupComp pSearchSoftwareSort = new CmbSoftwareGroupComp();
-		pSearchSoftwareSort.setBounds(168, 34, 120, 30);
+		pSearchSoftwareSort.setBounds(12, 10, 120, 30);
 		pOrderSearch.add(pSearchSoftwareSort);
 
 		CmbAdminGroupComp pAdminGroup = new CmbAdminGroupComp();
-		pAdminGroup.setBounds(325, 34, 120, 30);
+		pAdminGroup.setBounds(144, 10, 120, 30);
 		pOrderSearch.add(pAdminGroup);
 
 		StartAndEndDate pDate = new StartAndEndDate();
 		pDate.setBackground(Color.WHITE);
-		pDate.setBounds(488, 10, 690, 43);
+		pDate.setBounds(408, 0, 434, 43);
 		pOrderSearch.add(pDate);
 
 		CmbStringComp pSortUserNameSWName = new CmbStringComp();
-		pSortUserNameSWName.setBounds(493, 54, 120, 30);
+		pSortUserNameSWName.setBounds(276, 10, 120, 30);
 		pOrderSearch.add(pSortUserNameSWName);
 
 		TfBtnSearchComp pSearch = new TfBtnSearchComp("검색");
-		pSearch.setBounds(638, 54, 540, 30);
+		pSearch.setBounds(848, 10, 340, 30);
 		pOrderSearch.add(pSearch);
 	}
 
