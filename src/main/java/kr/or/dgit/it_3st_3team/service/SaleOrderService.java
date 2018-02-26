970d2d39e7099075dd5ea74e808549ec0b1b8965
HyeSeon Yang <skcm19@naver.com>
2018-02-26 16:31:54 +0900

    SaleOrderService

---
+++ src/main/java/kr/or/dgit/it_3st_3team/service/SaleOrderService.java
@@ -0,0 +1,10 @@
+package kr.or.dgit.it_3st_3team.service;
+
+import org.apache.ibatis.logging.Log;
+
+import com.mysql.jdbc.log.LogFactory;
+
+public class SaleOrderService {
+	private static final SaleOrderService instance = new SaleOrderService();
+	private static final Log log = LogFactory.
+}
