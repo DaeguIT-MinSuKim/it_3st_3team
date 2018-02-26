81e6417f0a4d46fd3594a0000f6b7fd2b433e7ac
HyeSeon Yang <skcm19@naver.com>
2018-02-26 18:13:28 +0900

    수정

--- src/main/java/kr/or/dgit/it_3st_3team/service/SaleOrderService.java
+++ src/main/java/kr/or/dgit/it_3st_3team/service/SaleOrderService.java
@@ -2,15 +2,18 @@ package kr.or.dgit.it_3st_3team.service;
 
 import org.apache.ibatis.logging.Log;
 import org.apache.ibatis.logging.LogFactory;
+import org.apache.ibatis.session.SqlSession;
+
+import kr.or.dgit.it_3st_3team.dto.SaleOrder;
+import kr.or.dgit.it_3st_3team.dto.User;
+import kr.or.dgit.it_3st_3team.ui.util.MyBatisSqlSessionFactory;
 
 
 
 public class SaleOrderService {
 	private static final SaleOrderService instance = new SaleOrderService();
 	private static final Log log = LogFactory.getLog(SaleOrderService.class);
-	private final String namespace = "kr.or.dgit.it_3st_3team.dao.SaleOrderService.";
-			
-			
+	private final String namespace = "kr.or.dgit.it_3st_3team.dao.SaleOrderDao.";
 			
 	public static SaleOrderService getInstance() {
 		return instance;
@@ -20,4 +23,17 @@ public class SaleOrderService {
 	
 	}
 	
+	public SaleOrder findSaleOrderByNo(SaleOrder ordNo) {
+		log.debug("findSaleOrderByNo()");
+		try (SqlSession sqlSession = MyBatisSqlSessionFactory.openSession();) {
+			return sqlSession.selectOne(namespace + "selectSaleOrderByNo", ordNo);
+		}
+	}
+	
+	public SaleOrder findSaleOrderWithAllByNo(SaleOrder ordNo) {
+		log.debug("findSaleOrderWithAllByNo()");
+		try (SqlSession sqlSession = MyBatisSqlSessionFactory.openSession();) {
+			return sqlSession.selectOne(namespace + "selectSaleOrderWithAllByNo", ordNo);
+		}
+	}
 }
