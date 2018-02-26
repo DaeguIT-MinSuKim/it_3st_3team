142a1d7100b0d18bea3f84b4607373205dc819c4
HyeSeon Yang <skcm19@naver.com>
2018-02-26 16:39:59 +0900

    SaleOrder 서비스

--- src/main/java/kr/or/dgit/it_3st_3team/service/SaleOrderService.java
+++ src/main/java/kr/or/dgit/it_3st_3team/service/SaleOrderService.java
@@ -1,10 +1,23 @@
 package kr.or.dgit.it_3st_3team.service;
 
 import org.apache.ibatis.logging.Log;
+import org.apache.ibatis.logging.LogFactory;
 
-import com.mysql.jdbc.log.LogFactory;
+
 
 public class SaleOrderService {
 	private static final SaleOrderService instance = new SaleOrderService();
-	private static final Log log = LogFactory.
+	private static final Log log = LogFactory.getLog(SaleOrderService.class);
+	private final String namespace = "kr.or.dgit.it_3st_3team.dao.SaleOrderService.";
+			
+			
+			
+	public static SaleOrderService getInstance() {
+		return instance;
+	}
+
+	public SaleOrderService() {
+	
+	}
+	
 }
