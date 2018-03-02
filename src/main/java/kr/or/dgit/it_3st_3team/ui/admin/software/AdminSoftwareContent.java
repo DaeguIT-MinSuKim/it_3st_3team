e41cdbc7347a4ad4214c82e466e49be0b47d7243
GangDae Lee <enhoomin@naver.com>
2018-03-02 18:28:51 +0900

    소프트웨어 등록 부분

--- src/main/java/kr/or/dgit/it_3st_3team/ui/admin/software/AdminSoftwareContent.java
+++ src/main/java/kr/or/dgit/it_3st_3team/ui/admin/software/AdminSoftwareContent.java
@@ -13,12 +13,14 @@ import kr.or.dgit.it_3st_3team.ui.component.PagingComp;
 import kr.or.dgit.it_3st_3team.ui.table.AdminSoftwareTable;
 
 @SuppressWarnings("serial")
-public class AdminSoftwareContent extends JPanel {
+public class AdminSoftwareContent extends JPanel implements ActionListener {
 
 	private AdminSoftwareRegister pRegister;
 	private AdminSoftwareSearch pSearch;
 	private AdminSoftwareTable pTable;
-
+	private JButton btnRegister;
+	private JButton btnCancel;
+	private JButton btnSearch;
 
 	public AdminSoftwareContent() {
 
@@ -48,55 +50,60 @@ public class AdminSoftwareContent extends JPanel {
 		pPageing.setBackground(new Color(240, 240, 240));
 		pPageing.setBounds(0, 814, 1190, 52);
 		add(pPageing);
-		
-		JButton btnRegister = new JButton("등록");
-		btnRegister.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				String f = pRegister.getpCompany().getCmbBox();
-				String a = pRegister.getpSWsort().getCmbBox();
-				String b = pRegister.getpSWName().getTfText();
-				String c = pRegister.getpSupplyPrice().getTfText();
-				String d = pRegister.getpSalePrice().getTfText();
-				System.out.println(a);
-				System.out.println(b);
-				System.out.println(c);
-				System.out.println(d);
-				if(b.equals("")) {
-					JOptionPane.showMessageDialog(null, "입력해주세요");
-					return;
-				}
-				
-				
-			}
-		});
-		
+
+		btnRegister = new JButton("등록");
+		btnRegister.addActionListener(this);
+
 		btnRegister.setBounds(967, 162, 97, 23);
 		add(btnRegister);
-		
-		JButton btnCancel = new JButton("취소");
+
+		btnCancel = new JButton("취소");
+		btnCancel.addActionListener(this);
 		btnCancel.setBounds(1076, 162, 97, 23);
 		add(btnCancel);
-		
-		JButton btnSearch = new JButton("검색");
+
+		btnSearch = new JButton("검색");
+		btnSearch.addActionListener(this);
 		btnSearch.setBounds(985, 206, 97, 23);
 		add(btnSearch);
 	}
-	
-	
-	public AdminSoftwareRegister getpRegister() {
-		return pRegister;
+
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == btnSearch) {
+			actionPerformedBtnSearch(e);
+		}
+		if (e.getSource() == btnCancel) {
+			actionPerformedBtnCancel(e);
+		}
+		if (e.getSource() == btnRegister) {
+			actionPerformedBtnRegister(e);
+		}
 	}
 
-	public AdminSoftwareSearch getpSearch() {
-		return pSearch;
-	}
-	
+	// 등록버튼
+	protected void actionPerformedBtnRegister(ActionEvent e) {
+		String f = pRegister.getpCompany().getCmbBox();
+		String a = pRegister.getpSWsort().getCmbBox();
+		String b = pRegister.getpSWName().getTfText();
+		String c = pRegister.getpSupplyPrice().getTfText();
+		String d = pRegister.getpSalePrice().getTfText();
+		System.out.println(f);
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		if (b.equals("")) {
+			JOptionPane.showMessageDialog(null, "입력해주세요");
+			return;
+		}
 
-	public void setpRegister(AdminSoftwareRegister pRegister) {
-		this.pRegister = pRegister;
 	}
 
-	
-	
-	
+	// 취소버튼
+	protected void actionPerformedBtnCancel(ActionEvent e) {
+	}
+
+	// 검색버튼
+	protected void actionPerformedBtnSearch(ActionEvent e) {
+	}
 }
