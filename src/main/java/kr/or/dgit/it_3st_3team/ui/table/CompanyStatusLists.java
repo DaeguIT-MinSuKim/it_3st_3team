71f60228d885aaaa4c20966af4152412fd00321d
HyeSeon Yang <skcm19@naver.com>
2018-03-05 22:57:00 +0900

    공급회사 테이블추가 update

--- src/main/java/kr/or/dgit/it_3st_3team/ui/table/CompanyStatusLists.java
+++ src/main/java/kr/or/dgit/it_3st_3team/ui/table/CompanyStatusLists.java
@@ -1,92 +1,36 @@
 package kr.or.dgit.it_3st_3team.ui.table;
 
-import java.awt.BorderLayout;
+import java.util.List;
 
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
-import javax.swing.ListSelectionModel;
 import javax.swing.SwingConstants;
-import javax.swing.table.DefaultTableCellRenderer;
-import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableColumnModel;
 
-public class CompanyStatusLists extends JPanel {
-	protected JTable table;
-	
-	public CompanyStatusLists() {
+import kr.or.dgit.it_3st_3team.dto.SaleOrder;
+import kr.or.dgit.it_3st_3team.ui.component.AbtractTableComp;
 
-		initComponents();
+public class CompanyStatusLists extends AbtractTableComp<SaleOrder> {
+
+	@Override
+	public void setTableAlignWidth() {
+		setTableCellAlign(SwingConstants.CENTER, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+		setTableRowHeight(30);
+
+		setTableCellWidth(400, 500, 300, 500, 400, 500, 500, 500, 500, 500);
 	}
 
-	private void initComponents() {
-		setLayout(new BorderLayout(0, 0));
-
-		JScrollPane scrollPane = new JScrollPane();
-		add(scrollPane, BorderLayout.CENTER);
-
-		table = new JTable();
-		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		loadDatas();
-		scrollPane.setViewportView(table);
+	@Override
+	public Object[] getColumnNames() {
+		return new String[] { "주문번호", "분류", "품목명", "주문수량","공급가격","판매가격", "부가세액","판매이윤","결제수단","날짜"};
 	}
 
-	private void loadDatas() {
-		NonEditableModel model = new NonEditableModel(getRows(), getColumNames());
-		table.setModel(model);
-		setAlignWidth();
-
-	}
-
-	private Object[][] getRows() {
-		return new Object[][] {
-			{"1", "게임", "영업1팀", "오버워치", "3", "120,000원","150,000원","계좌이체","100,000원","2018-02-23","100,000원"},
-			{"2", "게임", "영업1팀", "오버워치", "3", "120,000원","150,000원","계좌이체","100,000원","2018-02-23","100,000원"},
-			{"3", "게임", "영업1팀", "오버워치", "3", "120,000원","150,000원","계좌이체","100,000원","2018-02-23","100,000원"}
-		};
-	}
-
-	public String[] getColumNames() {
-
-		return new String[] { "번호", "분류", "관리자", "품목명", "주문수량", "공급가격","판매가격","결제수단","매출금","날짜","판매이윤"};
-	}
-
-	private void setAlignWidth() {
-		// 셀의 너비와 정렬
-		tableCellAlign(SwingConstants.CENTER, 0, 1, 2, 3, 4, 5, 6,7,8,9,10);
-
-		tableCellWidth(200, 300, 400, 500, 200, 500, 500, 500, 500, 500, 500);
-
-	}
-
-	protected void tableCellWidth(int... width) {
-		TableColumnModel model = table.getColumnModel();
-		for (int i = 0; i < width.length; i++) {
-			model.getColumn(i).setPreferredWidth(width[i]);
+	@Override
+	public Object[][] getRows(List<SaleOrder> list) {
+		Object[][] rows = null;
+		rows = new Object[list.size()][];
+		for(int i=0; i<list.size(); i++) {
+			rows[i] = list.get(i).toArraySalesLists();
 		}
-
-	}
-
-	protected void tableCellAlign(int align, int... idx) {
-		DefaultTableCellRenderer dtcr = new DefaultTableCellRenderer();
-		dtcr.setHorizontalAlignment(align);
-
-		TableColumnModel model = table.getColumnModel();
-		for (int i = 0; i < idx.length; i++) {
-			model.getColumn(idx[i]).setCellRenderer(dtcr);
-		}
-	}
-
-	// 테이블 내용을 수정하지 못하게 하는 클래스
-	class NonEditableModel extends DefaultTableModel {
-		public NonEditableModel(Object[][] data, Object[] columnNames) {
-			super(data, columnNames);
-		}
-
-		@Override
-		public boolean isCellEditable(int row, int column) {
-			return false;
-		}
+		return rows;
 	}
 
 }
