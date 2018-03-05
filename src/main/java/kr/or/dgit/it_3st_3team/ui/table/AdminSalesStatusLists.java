c753e0f2877231cfc2efa56f7c387f36bf5e8b35
HyeSeon Yang <skcm19@naver.com>
2018-03-05 18:46:26 +0900

    영업사원 테이블리스트

---
+++ src/main/java/kr/or/dgit/it_3st_3team/ui/table/AdminSalesStatusLists.java
@@ -0,0 +1,40 @@
+package kr.or.dgit.it_3st_3team.ui.table;
+
+import java.util.List;
+
+import javax.swing.SwingConstants;
+
+import kr.or.dgit.it_3st_3team.dto.SaleOrder;
+import kr.or.dgit.it_3st_3team.ui.component.AbtractTableComp;
+
+@SuppressWarnings("serial")
+public class AdminSalesStatusLists extends AbtractTableComp<SaleOrder> {
+
+	@Override
+	public void setTableAlignWidth() {
+		// 셀의 너비와 정렬
+		setTableCellAlign(SwingConstants.CENTER, 0, 1, 2, 3, 4, 5, 6,7,8,9,10,11);
+		
+		setTableRowHeight(30);
+		
+		setTableCellWidth(400, 500, 300, 500, 400, 500, 500, 500, 500, 500, 500, 600);		
+	}
+
+	@Override
+	public Object[] getColumnNames() {
+		return new String[] { "주문번호", "고객명", "분류", "품목명", "주문수량","공급가격","판매가격", "부가세액","판매이윤","결제수단","공급회사명","날짜"};
+
+	}
+
+	@Override
+	public Object[][] getRows(List<SaleOrder> list) {
+		Object[][] rows = null;
+		rows = new Object[list.size()][];
+		for(int i=0; i<list.size(); i++) {
+			rows[i] = list.get(i).toArraySalesLists();
+		}
+		return rows;
+	}
+	
+
+}
